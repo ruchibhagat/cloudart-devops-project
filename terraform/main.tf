430bb0f6f4a7269831ae38b143753e1ae5ab7911
Ruchi <ruchi.bhagat037@gmail.com>
2026-09-22 11:26:23 -0500

    Remove unused AMI data source

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,21 +12,6 @@ provider "aws" {
   region = var.aws_region
 }
 
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-
-  owners = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["al2023-ami-*-x86_64"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
 resource "aws_security_group" "cloudcart" {
   name        = "cloudcart-sg"
   description = "Security group for CloudCart EC2 instance"
