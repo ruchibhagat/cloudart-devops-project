0d5c7fba109cd616dad0175d7fedd2954c71a42c
Ruchi <ruchi.bhagat037@gmail.com>
2026-09-23 11:26:52 -0500

    Add Terraform variable configuration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,23 +1,19 @@
 variable "aws_region" {
   description = "AWS region where CloudCart infrastructure will be created"
   type        = string
-  default     = "ca-central-1"
 }
 
 variable "instance_type" {
   description = "Ec2 instance type for cloudcart"
   type        = string
-  default     = "t3.micro"
 }
 
 variable "key_name" {
   description = "EC2 key pair name"
   type        = string
-  default     = "cloudcartkey"
 }
 
 variable "allowed_ssh_cidr" {
   description = "CIDR block allowed to access SSH"
   type        = string
-  default     = "0.0.0.0/0"
 }
